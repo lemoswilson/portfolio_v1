49636eca43d1843321c20a7327c22019d8ec9b71
Wilson Lemos <wilson.lemos@bukwild.com>
2023-08-15 06:33:07 -0700

    making it blank

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,93 +34,94 @@ export const linkedin = "https://linkedin.com/lemoswilson"
 export const github = "https://github.com/lemoswilson"
 
 export default function Home({data}: HomeProps) {
-  const [projectModal, setProjectModal] = useState<string>('');
-  const [isMenuOpen, setMenu] = useState(false);
-  const [hide, setHide] = useState(false);
+  // const [projectModal, setProjectModal] = useState<string>('');
+  // const [isMenuOpen, setMenu] = useState(false);
+  // const [hide, setHide] = useState(false);
 
-  const hero_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const about_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const experience_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const work_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const contact_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const ref_da_ref = useRef({about: about_ref, experience: experience_ref, work: work_ref, contact: contact_ref})
+  // const hero_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
+  // const about_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
+  // const experience_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
+  // const work_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
+  // const contact_ref = useRef() as React.MutableRefObject<HTMLDivElement>;
+  // const ref_da_ref = useRef({about: about_ref, experience: experience_ref, work: work_ref, contact: contact_ref})
 
-  const [loadingDone, setLoadingDone] = useState(false);
-  const [typingDone, setTyping] = useState(false);
-  const [socialsDone, setSocialsLoading] = useState(false);
-  const [emailDone, setEmailDone] = useState(false);
+  // const [loadingDone, setLoadingDone] = useState(false);
+  // const [typingDone, setTyping] = useState(false);
+  // const [socialsDone, setSocialsLoading] = useState(false);
+  // const [emailDone, setEmailDone] = useState(false);
 
-  function setLoading(state: boolean){
-    setLoadingDone(state);
-  }
+  // function setLoading(state: boolean){
+  //   setLoadingDone(state);
+  // }
 
-  useBlockOverflow(isMenuOpen, projectModal.length > 0)
-  useKeyboardCloseModal(projectModal, closeMenu)
+  // useBlockOverflow(isMenuOpen, projectModal.length > 0)
+  // useKeyboardCloseModal(projectModal, closeMenu)
 
-  function closeMenu(){
-    setProjectModal('')
-    setMenu(false);
-  }
+  // function closeMenu(){
+  //   setProjectModal('')
+  //   setMenu(false);
+  // }
 
-  function scrollToElement(destination: string){
-    scroll({top: ref_da_ref.current[destination].current.offsetTop - 70, behavior: 'smooth'});
-  }
+  // function scrollToElement(destination: string){
+  //   scroll({top: ref_da_ref.current[destination].current.offsetTop - 70, behavior: 'smooth'});
+  // }
 
+  return (<div></div>)
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Wilson Lemos</title>
-        <link rel="icon" href="/favicon-16x16.png" />
-        <link rel="stylesheet" href="https://use.typekit.net/sux7xms.css"></link>
-        <script dangerouslySetInnerHTML={{  __html: JSON.stringify(structuredData) }} type="application/ld+json"/>
-      </Head>
+  // return (
+  //   <div className={styles.container}>
+  //     <Head>
+  //       <title>Wilson Lemos</title>
+  //       <link rel="icon" href="/favicon-16x16.png" />
+  //       <link rel="stylesheet" href="https://use.typekit.net/sux7xms.css"></link>
+  //       <script dangerouslySetInnerHTML={{  __html: JSON.stringify(structuredData) }} type="application/ld+json"/>
+  //     </Head>
 
-      <Menu 
-        isMenuOpen={isMenuOpen} 
-        scrollToElement={scrollToElement} 
-        setHide={setHide} 
-        setMenu={setMenu}  
-      />
-      <NavBar 
-        emailDone={emailDone} 
-        scrollToElement={scrollToElement} 
-        setMenu={setMenu} 
-      />
-      <Overlay 
-        closeMenus={closeMenu} 
-        isMenuOpen={isMenuOpen}
-        isModalOpen={projectModal.length > 0}
-        hide={hide}
-      />
-      { socialsDone ?  <Email email={email} setEmailDone={setEmailDone}/> : null }
-      { typingDone ? <Socials setSocialsDone={setSocialsLoading}/> : null }
-      <ProjectModal closeMenu={closeMenu} data={data} name={projectModal}/>
+  //     <Menu 
+  //       isMenuOpen={isMenuOpen} 
+  //       scrollToElement={scrollToElement} 
+  //       setHide={setHide} 
+  //       setMenu={setMenu}  
+  //     />
+  //     <NavBar 
+  //       emailDone={emailDone} 
+  //       scrollToElement={scrollToElement} 
+  //       setMenu={setMenu} 
+  //     />
+  //     <Overlay 
+  //       closeMenus={closeMenu} 
+  //       isMenuOpen={isMenuOpen}
+  //       isModalOpen={projectModal.length > 0}
+  //       hide={hide}
+  //     />
+  //     { socialsDone ?  <Email email={email} setEmailDone={setEmailDone}/> : null }
+  //     { typingDone ? <Socials setSocialsDone={setSocialsLoading}/> : null }
+  //     <ProjectModal closeMenu={closeMenu} data={data} name={projectModal}/>
 
-      <main className={styles.main}>
-        <Hero 
-          loadingDone={loadingDone} 
-          setLoading={setLoading} 
-          setTyping={setTyping} 
-          scrollToElement={scrollToElement} 
-          ref={hero_ref} 
-        />
-        <About ref={about_ref}/> 
-        <Experience ref={experience_ref}/>
-        <Work 
-          data={data} 
-          projectModal={projectModal} 
-          setProjectModal={setProjectModal}
-          ref={work_ref}
-        />
-        <Contact ref={contact_ref} />
-      </main>
+  //     <main className={styles.main}>
+  //       <Hero 
+  //         loadingDone={loadingDone} 
+  //         setLoading={setLoading} 
+  //         setTyping={setTyping} 
+  //         scrollToElement={scrollToElement} 
+  //         ref={hero_ref} 
+  //       />
+  //       <About ref={about_ref}/> 
+  //       <Experience ref={experience_ref}/>
+  //       <Work 
+  //         data={data} 
+  //         projectModal={projectModal} 
+  //         setProjectModal={setProjectModal}
+  //         ref={work_ref}
+  //       />
+  //       <Contact ref={contact_ref} />
+  //     </main>
 
-      <footer className={styles.footer}>
-        Designed &amp; Developed by Wilson Lemos
-      </footer>
-    </div>
-  )
+  //     <footer className={styles.footer}>
+  //       Designed &amp; Developed by Wilson Lemos
+  //     </footer>
+  //   </div>
+  // )
 }
 
 export const getStaticProps = async () => {
